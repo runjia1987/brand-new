fd42d87e774fee336cb6db5854f5a02e41a861ab
runjia1987 <runjiamail@gmail.com>
2017-03-23 22:43:11 +0800

    mysql 5.7.10 onlt supports TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, but  datetime column does not have feature of DEFAULT CURRENT_TIMESTAMP

--- create.sql
+++ create.sql
@@ -9,8 +9,8 @@ create table brand (
 	`creator` varchar(30) NOT NULL DEFAULT '' COMMENT 'creator name',
 	`business` varchar(200) NOT NULL DEFAULT '' COMMENT 'business',
 	`head_quarter` VARCHAR(30) NOT NULL COMMENT 'head quarter address',
-	`create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-	`update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+	`create_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+	`update_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='品牌表';
 create UNIQUE index uniq_name_brand_idx on brand(`name`);
@@ -22,8 +22,8 @@ create table shop (
 	`address` varchar(150) NOT NULL DEFAULT '' COMMENT 'shop address',
 	`manager` varchar(100) NOT NULL DEFAULT '' COMMENT 'manager name',
 	`status` TINYINT NOT NULL DEFAULT 0 COMMENT 'shop status, 0非在营,1在营,2不明确,3计划关闭',
-	`create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-	`update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+	`create_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+	`update_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 	PRIMARY KEY (`id`),
 	FOREIGN KEY (`brand_id`) REFERENCES brand(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='店铺表';
